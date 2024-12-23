9c651d87754fe688de5220e4dc89f9a377f82203
Sofia Lee <sofia819@gmail.com>
2024-12-22 20:16:39 -0500

    Set up cors (#8)
    ## Description
    Set up cors for specific App URL.

--- index.ts
+++ index.ts
@@ -7,12 +7,23 @@ import cors from '@fastify/cors';
 const fastify = Fastify({
   logger: true,
 });
-const port: number =
-  Number.parseInt(dotenv.config()?.parsed?.PORT || '') || 5000;
+const port = Number.parseInt(dotenv.config()?.parsed.PORT);
+const appUrl = dotenv.config()?.parsed?.APP_URL;
 const host = 'RENDER' in process.env ? `0.0.0.0` : `localhost`;
 
 fastify.register(websocket);
-fastify.register(cors);
+fastify.register(cors, {
+  origin: (origin, cb) => {
+    //  Request from appUrl will pass
+    if (origin.startsWith(appUrl)) {
+      cb(null, true);
+      return;
+    }
+
+    // Generate an error on other origins, disabling access
+    cb(new Error('Not allowed'), false);
+  },
+});
 fastify.register(route, { prefix: '/' });
 
 fastify.listen({ port, host }, (err: Error, address: string) => {
